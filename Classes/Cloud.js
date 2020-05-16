45ada9b99e868950671fae9eaf69af4bb5444e32
Angelo Carlotto <angelocarlotto@Angelos-MacBook-Pro.local>
2020-05-16 10:59:30 -0400

    transcript all code to EC6 and organized everything in classes

---
+++ Classes/Cloud.js
@@ -0,0 +1,95 @@
+import {getRandomNum} from './Utils.js'
+import {IS_HIDPI} from './Config.js'
+import Runner from './Runner.js'
+export default class Cloud{
+
+          /**
+   * Cloud object config.
+   * @enum {number}
+   */
+  static config = {
+    HEIGHT: 14,
+    MAX_CLOUD_GAP: 400,
+    MAX_SKY_LEVEL: 30,
+    MIN_CLOUD_GAP: 100,
+    MIN_SKY_LEVEL: 71,
+    WIDTH: 46
+};
+
+
+constructor(canvas, spritePos, containerWidth){
+
+
+    this.canvas = canvas;
+    this.canvasCtx = this.canvas.getContext('2d');
+    this.spritePos = spritePos;
+    this.containerWidth = containerWidth;
+    this.xPos = containerWidth;
+    this.yPos = 0;
+    this.remove = false;
+    this.cloudGap = getRandomNum(Cloud.config.MIN_CLOUD_GAP,
+        Cloud.config.MAX_CLOUD_GAP);
+
+
+        
+    this.init();
+}
+
+
+    /**
+     * Initialise the cloud. Sets the Cloud height.
+     */
+    init () {
+        this.yPos = getRandomNum(Cloud.config.MAX_SKY_LEVEL,
+            Cloud.config.MIN_SKY_LEVEL);
+        this.draw();
+    }
+
+    /**
+     * Draw the cloud.
+     */
+    draw () {
+        this.canvasCtx.save();
+        var sourceWidth = Cloud.config.WIDTH;
+        var sourceHeight = Cloud.config.HEIGHT;
+
+        if (IS_HIDPI) {
+            sourceWidth = sourceWidth * 2;
+            sourceHeight = sourceHeight * 2;
+        }
+
+        this.canvasCtx.drawImage(Runner.imageSprite, this.spritePos.x,
+            this.spritePos.y,
+            sourceWidth, sourceHeight,
+            this.xPos, this.yPos,
+            Cloud.config.WIDTH, Cloud.config.HEIGHT);
+
+        this.canvasCtx.restore();
+    }
+
+    /**
+     * Update the cloud position.
+     * @param {number} speed
+     */
+    update (speed) {
+        if (!this.remove) {
+            this.xPos -= Math.ceil(speed);
+            this.draw();
+
+            // Mark as removeable if no longer in the canvas.
+            if (!this.isVisible()) {
+                this.remove = true;
+            }
+        }
+    }
+
+    /**
+     * Check if the cloud is visible on the stage.
+     * @return {boolean}
+     */
+    isVisible () {
+        return this.xPos + Cloud.config.WIDTH > 0;
+    }
+
+
+ }
